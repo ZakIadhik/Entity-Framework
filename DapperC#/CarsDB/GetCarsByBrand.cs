7214dda82af510471ba287753a37db1059366e33
ZakIadhik <nazarov.ramin.2004@mail.ru>
2025-02-22 22:03:08 +0400

    Add files via upload

---
+++ DapperC#/CarsDB/GetCarsByBrand.cs
@@ -0,0 +1,20 @@
+﻿using CarsDB;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+public static class GetCarsByBrand
+{
+    public static List<ObjectCar> GetCarsByBrand1(string connectionString, string brandName)
+    {
+        var commandString = "SELECT * FROM Cars WHERE Brand = @BrandName";
+
+        using (var connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+                
+            var cars = connection.Query<ObjectCar>(commandString, new { BrandName = brandName }).AsList();
+
+            return cars;
+        }
+    }
+}
