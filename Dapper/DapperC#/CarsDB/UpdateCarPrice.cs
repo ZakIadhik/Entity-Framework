8478b2d820acb598fd69cf2be2bec2700ff04f88
ZakIadhik <nazarov.ramin.2004@mail.ru>
2025-02-23 17:02:13 +0400

    Add files via upload

---
+++ Dapper/DapperC#/CarsDB/UpdateCarPrice.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+
+public static class UpdateCarPrice
+{
+    public static void UpdateCarPrice1(int carId, decimal newPrice, string connectionString)
+    {
+        var commandString = "UPDATE Cars SET Price = @NewPrice WHERE Id = @carId";
+
+        using (var connection = new SqlConnection(connectionString))
+        {
+
+            connection.Open();
+                
+            var rowsAffected = connection.Execute(commandString, new {NewPrice = newPrice, CarId = carId});
+
+            if (rowsAffected > 0)
+            {
+                Console.WriteLine("Car Price Updated");
+            }
+            else
+            {
+                Console.WriteLine("Car Price NOT Updated");
+            }
+        }
+    }
+}
